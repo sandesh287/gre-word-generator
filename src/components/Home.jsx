ff7f3161087036ce522f62c9956c8232a1e1f2fc
Sandesh Panthee <sangm3138@gmail.com>
2023-12-18 13:49:04 +0545

    firstpush

---
+++ src/components/Home.jsx
@@ -0,0 +1,137 @@
+import "./home.css";
+import React, { useState, useEffect } from 'react';
+
+function Home() {
+    const [wordDataset, setWordDataset] = useState(null);
+    const [currentRandomWordIndex, setCurrentRandomWordIndex] = useState(null);
+    const [randomWord, setRandomWord] = useState('Click the button to generate a random word');
+    const [wordPOS, setWordPOS] = useState('Word part of speech will be displayed here');
+    const [wordRoot, setWordRoot] = useState('Word root meaning will be displayed here');
+    const [wordMeaning, setWordMeaning] = useState('Word meaning will be displayed here');
+    const [wordExample, setWordExample] = useState('Word example will be displayed here');
+    const [wordSynonym, setWordSynonym] = useState('Word synonym will be displayed here');
+    const [wordAntonym, setWordAntonym] = useState('Word antonym will be displayed here');
+
+    useEffect(() => {
+        // Load the dataset when the component mounts
+        loadDataset();
+    }, []);
+
+    const loadDataset = async () => {
+        try {
+            const response = await fetch('words.json'); // Adjust the path based on your project structure
+            const data = await response.json();
+            setWordDataset(data);
+        } catch (error) {
+        console.error('Error loading dataset:', error.message);
+        }
+    };
+
+    const generateRandomWord = () => {
+        if (!wordDataset) {
+            console.error('Dataset not loaded');
+            return;
+        }
+
+        // Get a random index from the dataset
+        const randomIndex = Math.floor(Math.random() * wordDataset.length);
+
+        // Get the word at the random index
+        const randomWord = wordDataset[randomIndex].word;
+
+        // Update the state
+        setCurrentRandomWordIndex(randomIndex);
+        setRandomWord(`Random Word: ${randomWord}`);
+        setWordPOS('Word part of speech will be displayed here');
+        setWordRoot('Word root meaning will be displayed here');
+        setWordMeaning('Word meaning will be displayed here');
+        setWordExample('Word example will be displayed here');
+        setWordSynonym('Word synonym will be displayed here');
+        setWordAntonym('Word antonym will be displayed here');
+    };
+
+    const displayWordPOS = () => {
+        if (currentRandomWordIndex !== null) {
+            const pos = wordDataset[currentRandomWordIndex].pos;
+            setWordPOS(`Part of speech: ${pos}`);
+        } else {
+            console.error('No word generated yet');
+        }
+    };
+
+    const displayWordRoot = () => {
+        if (currentRandomWordIndex !== null) {
+            const root = wordDataset[currentRandomWordIndex].root;
+            setWordRoot(`Root: ${root}`);
+        } else {
+            console.error('No word generated yet');
+        }
+    };
+
+    const displayWordMeaning = () => {
+        if (currentRandomWordIndex !== null) {
+            const meaning = wordDataset[currentRandomWordIndex].meaning;
+            setWordMeaning(`Meaning: ${meaning}`);
+        } else {
+            console.error('No word generated yet');
+        }
+    };
+
+    const displayWordExample = () => {
+        if (currentRandomWordIndex !== null) {
+            const example = wordDataset[currentRandomWordIndex].example;
+            setWordExample(`Example: ${example}`);
+        } else {
+            console.error('No word generated yet');
+        }
+    };
+
+    const displayWordSynonym = () => {
+        if (currentRandomWordIndex !== null) {
+        const synonym = wordDataset[currentRandomWordIndex].synonym;
+        setWordSynonym(`Synonym: ${synonym}`);
+        } else {
+        console.error('No word generated yet');
+        }
+    };
+
+    const displayWordAntonym = () => {
+        if (currentRandomWordIndex !== null) {
+            const antonym = wordDataset[currentRandomWordIndex].antonym;
+            setWordAntonym(`Antonym: ${antonym}`);
+        } else {
+            console.error('No word generated yet');
+        }
+    };
+
+  return (
+    <div>
+        <h1>GRE Word Generator</h1>
+        <p id="random-word">{randomWord}</p>
+        <button onClick={generateRandomWord}>Generate Word</button>
+
+        <p id="word-pos">{wordPOS}</p>
+        <button onClick={displayWordPOS}>Show Part of Speech</button>
+
+        <p id="word-root">{wordRoot}</p>
+        <button onClick={displayWordRoot}>Show Root Meaning</button>
+
+        <p id="word-meaning">{wordMeaning}</p>
+        <button onClick={displayWordMeaning}>Show Meaning</button>
+
+        <p id="word-example">{wordExample}</p>
+        <button onClick={displayWordExample}>Show Example</button>
+
+        <p id="word-synonym">{wordSynonym}</p>
+        <button onClick={displayWordSynonym}>Show Synonyms</button>
+
+        <p id="word-antonym">{wordAntonym}</p>
+        <button onClick={displayWordAntonym}>Show Antonym</button>
+
+        <p>Copyright © 2023 BabaGREWords.com, Inc. • All Rights Reserved</p>
+
+    </div>
+  )
+}
+
+export default Home;
